6f4a40644a61c9812540fedccd23564b5c44dad0
Faheem <frontendmakaidigitals@gmail.com>
2024-08-31 18:10:50 +0530

    nav done

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -60,43 +60,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
       return;
     }
   };
-  const components = [
-    {
-      title: "Alert Dialog",
-      href: "/docs/primitives/alert-dialog",
-      description:
-        "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-      title: "Hover Card",
-      href: "/docs/primitives/hover-card",
-      description:
-        "For sighted users to preview content available behind a link.",
-    },
-    {
-      title: "Progress",
-      href: "/docs/primitives/progress",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-      title: "Scroll-area",
-      href: "/docs/primitives/scroll-area",
-      description: "Visually or semantically separates content.",
-    },
-    {
-      title: "Tabs",
-      href: "/docs/primitives/tabs",
-      description:
-        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-      title: "Tooltip",
-      href: "/docs/primitives/tooltip",
-      description:
-        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-  ];
+ 
   return (
     <div className="menu-block-wrapper">
       <div
@@ -123,7 +87,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
           </div>
         </div>
         <ul className={`site-menu-main ${color}`}>
-          {/* Global navbar */}
+ 
           <NavigationMenu>
             <NavigationMenuList >
               <NavigationMenuItem>
@@ -131,10 +95,10 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                   Services
                 </NavigationMenuTrigger>
                 <NavigationMenuContent className="" >
-                  <div className="grid gap-10 px-6 py-10 md:w-[400px] lg:w-[900px] lg:grid-cols-3">
+                  <div className="grid gap-10 px-6 py-10 md:w-[400px] lg:w-[1100px] lg:grid-cols-3">
                     <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Data & Analytics</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Leveraging today’s data for a more promising future
                       </p>
                     </div>
@@ -142,29 +106,29 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Artificial Intelligence{" "}
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Shaping the future through advanced AI
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">
-                        Applied AI : Conventional and Generative AI
+                        Conventional and Generative AI
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Bridging conventional and generative AI to deliver
                         effective, real-world applications.
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Software engineering</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Crafting robust and scalable systems through expert
                         software engineering.
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Cloud modernisation</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Transforming business operations with next-generation
                         cloud modernization
                       </p>
@@ -173,21 +137,21 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Systems Implementation
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Transforming concepts into functional systems through
                         precise implementation
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Strategy</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Guiding growth and innovation with a clear strategic
                         vision
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Privacy & Security</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Safeguarding information with cutting-edge privacy and
                         security practices
                       </p>
@@ -196,7 +160,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Organizational Change{" "}
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Enhancing company approach with top level cultural &
                         hierarchy transformation
                       </p>
@@ -209,26 +173,26 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                   Industries
                 </NavigationMenuTrigger>
                 <NavigationMenuContent>
-                  <div className="grid gap-10 px-6 py-10 md:w-[400px] lg:w-[900px] lg:grid-cols-3">
+                  <div className="grid gap-10 px-6 py-10 md:w-[400px] lg:w-[1100px] lg:grid-cols-3">
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">
                         Life Science & Health Care
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Revolutionizing life sciences and Healthcare through
                         AI-powered research and diagnostics
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Finance</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Optimizing financial strategies and customer experiences
                         with AI
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Technology</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Leading the way with innovative technology and digital
                         transformation
                       </p>
@@ -237,7 +201,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Media and Communications
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Leveraging AI and ML to tailor communications and drive
                         impactful storytelling
                       </p>
@@ -246,7 +210,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Travel Transport and Logistics
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Deliver tangible business results by enhancing
                         profitability and customer experience in the TTL
                         industry
@@ -256,7 +220,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Educations and Learning
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Speed up modernization in the EdTech industry by
                         leveraging cloud-based infrastructure and advanced AI/ML
                         solutions to achieve excellence.
@@ -266,7 +230,7 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Retails and E-commerce
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         A robust selection of wide solutions equips retailers
                         with a competitive advantage, accelerates growth, and
                         promotes innovation.
@@ -276,14 +240,14 @@ const Navbar = ({ mobileMenu, setMobileMenu, color }) => {
                       <p className="text-md font-bold">
                         Manufacturing & Distribution
                       </p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Driving growth and efficiency in manufacturing and
                         distribution through smart technology and automation
                       </p>
                     </div>
                   <div className="p-3 hover:bg-slate-100 rounded-xl">
                       <p className="text-md font-bold">Resource and Wealth</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-sm font-medium mt-1">
                         Transforming resource and wealth strategies to unlock
                         new opportunities and drive financial success
                       </p>
