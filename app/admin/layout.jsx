ff3422af45d6e0bdaa7d6f776ff263140f9a116b
Faheem <frontendmakaidigitals@gmail.com>
2024-09-20 12:46:17 +0530

    dashboard added

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -67,7 +67,7 @@ const Sidebar = () => {
             className={`flex px-5 items-center  transiton-all duration-300 gap-3 py-4 ${
               path.toLowerCase() == menu.name.toLowerCase()
                 ? "bg-sky-500 text-gray-100"
-                : "hover:bg-blue-200 hover:text-gray-950 text-gray-100"
+                : "hover:bg-blue-200 hover:text-gray-900 text-gray-100"
             }`}
           >
             <div className="xl:text-xl xxl:text-3xl ">{menu.icon}</div>
