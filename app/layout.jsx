19683ef1de473b88eeac8264213fdf1acea2dba6
Faheem <frontendmakaidigitals@gmail.com>
2024-09-30 14:31:11 +0530

    dfdfd

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import Router from 'next/router';
+import Router from "next/router";
 import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import JOS from "jos-animation";
@@ -9,7 +9,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "../styles/globals.css";
 import "../styles/vendors/menu.css";
-
+import Loader from "./loading";
 
 const DMSans = localFont({
   src: "../fonts/DMSans-Bold.woff2",
@@ -56,22 +56,7 @@ export default function RootLayout({ children }) {
     JOS.refresh();
   }, [pathname]);
 
-
-  useEffect(() => {
-    const handleStart = () => setLoading(true);
-    const handleStop = () => setLoading(false);
-
-    Router.events.on('routeChangeStart', handleStart);
-    Router.events.on('routeChangeComplete', handleStop);
-    Router.events.on('routeChangeError', handleStop);
-
-    return () => {
-      Router.events.off('routeChangeStart', handleStart);
-      Router.events.off('routeChangeComplete', handleStop);
-      Router.events.off('routeChangeError', handleStop);
-    };
-  }, []);
-
+  
 
   return (
     <html lang="en">
@@ -89,7 +74,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${DMSans.variable} ${ClashDisplay.variable} ${Raleway.variable} ${SpaceGrotesk.variable} ${inter.variable}`}
       >
-         
+        { loading && <Loader /> }
         {children}
       </body>
     </html>
